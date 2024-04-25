0e77794ed3964dd3261ac1fc099eeeec3eaa48ba
GabrielCardosoLIma <limag656@gmail.com>
2024-04-25 17:02:29 -0300

    up(Login, Register and Recovery Password)

--- app/src/main/java/br/com/sentinellock/RegisterActivity.kt
+++ app/src/main/java/br/com/sentinellock/RegisterActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.Task
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class RegisterActivity : AppCompatActivity() {
 
+    // Declaração das variáveis necessárias
     private lateinit var functions: FirebaseFunctions
 
     private lateinit var buttonBackToSignIn: ImageButton
@@ -34,7 +39,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        // Inicialização do Firebase Functions
+        // Inicialização do Firebase Functions com a região South America
         functions = Firebase.functions("southamerica-east1")
 
         // Inicialização das visualizações
@@ -49,6 +54,7 @@ class RegisterActivity : AppCompatActivity() {
 
     // Inicializa as visualizações da interface do usuário
     private fun initializeViews() {
+        // Associa as variáveis às visualizações no layout XML
         buttonBackToSignIn = findViewById(R.id.buttonBackToSignIn)
         buttonRegister = findViewById(R.id.buttonRegister)
         buttonSignIn = findViewById(R.id.buttonSignIn)
@@ -75,9 +81,8 @@ class RegisterActivity : AppCompatActivity() {
 
         // Ouvinte para registrar o cliente
         buttonRegister.setOnClickListener {
-            // Verifica se os campos de entrada são válidos
             if (isValidInputs()) {
-                // Cria um objeto Cliente com os dados inseridos
+                // Cria um objeto cliente com os dados inseridos
                 val client = Client(
                     nome = eTextName.text.toString(),
                     email = eTextEmail.text.toString(),
@@ -87,29 +92,28 @@ class RegisterActivity : AppCompatActivity() {
                     dataNascimento = eTextAge.text.toString()
                 )
 
-                // Registra o cliente no Firebase
-                registerClient(client)
-                    .addOnCompleteListener { task ->
-                        val e = task.exception
-                        if (e != null) {
-                            if (e.message == "UNKNOWN") {
-                                // Exibe mensagem de sucesso
-                                showToast("Cadastrado com sucesso! Um e-mail de verificação foi enviado para o e-mail: ${client.email}.")
-                            } else {
-                                // Exibe mensagem de erro técnico
-                                showToast("Erro técnico! Entre em contato com o suporte.")
-                            }
-                        }
+                // Chama a função de registro em uma coroutine
+                CoroutineScope(Dispatchers.Main).launch {
+                    try {
+                        val result = registerClient(client)
+                        // Exibe uma mensagem de sucesso após o registro
+                        showAlert("Registro bem-sucedido: $result")
+                    } catch (e: Exception) {
+                        // Trata qualquer exceção ocorrida durante o registro
+                        Log.e(TAG, "Erro durante o registro: ${e.message}")
+                        showAlert("${e.message}")
                     }
+                }
             } else {
-                // Exibe mensagem se os campos não estiverem preenchidos
-                showToast("Preencha todos os campos!! Todos os campos são obrigatórios.")
+                // Exibe uma mensagem de erro se algum campo estiver vazio
+                showAlert("Preencha todos os campos!! Todos os campos são obrigatórios.")
             }
         }
     }
 
     // Configura as validações de entrada
     private fun setupInputValidations() {
+        // Configura os ouvintes de foco para validar os campos de entrada
         val emailLayout: TextInputLayout = findViewById(R.id.etEmail)
         val passwordLayout: TextInputLayout = findViewById(R.id.etPassword)
         val CPFLayout: TextInputLayout = findViewById(R.id.etCPF)
@@ -161,7 +165,8 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     // Registra o cliente no Firebase
-    private fun registerClient(client: Client): Task<String> {
+    private suspend fun registerClient(client: Client): String {
+        // Monta os dados do cliente para enviar para a função no Firebase
         val data = hashMapOf(
             "nome" to client.nome,
             "cpf" to client.cpf,
@@ -171,23 +176,48 @@ class RegisterActivity : AppCompatActivity() {
             "senha" to client.senha,
         )
 
-        return functions
+        // Chama a função de registro no Firebase Functions e aguarda a resposta
+        val result = functions
             .getHttpsCallable("funcCadastrarCliente")
             .call(data)
-            .continueWith { task ->
-                val result = task.result?.data as String
-                Log.d(TAG, result)
-                result
-            }
+            .await()
+
+        // Obtém os dados da resposta
+        val responseData = result.data
+
+        return if (responseData is String) {
+            // Retorna a resposta se for uma string
+            responseData
+        } else {
+            // Log de erro se a resposta não for uma string
+            Log.e(TAG, "Resposta inesperada do backend: $responseData")
+            "Erro ao processar resposta do servidor"
+        }
     }
 
-    // Exibe uma mensagem toast
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    // Método para exibir o AlertDialog
+    private fun showAlert(message: String) {
+        // Cria um AlertDialog com a mensagem fornecida
+        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialogStyle)
+        builder.setMessage(message)
+            .setPositiveButton("OK") { dialog, _ ->
+                // Se o registro for bem-sucedido, navega de volta para a atividade de login
+                if(message == "Registro bem-sucedido: Cadastro realizado com sucesso!"){
+                    navigateToLoginActivity()
+                }
+                dialog.dismiss()
+            }
+        val alertDialog = builder.create()
+        alertDialog.show()
+
+        // Personaliza a cor do botão positivo
+        val positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setTextColor(ContextCompat.getColor(this, R.color.green_500))
     }
 
     // Verifica se os campos de entrada são válidos
     private fun isValidInputs(): Boolean {
+        // Verifica se algum campo está vazio
         val name = eTextName.text.toString()
         val email = eTextEmail.text.toString()
         val password = eTextPassword.text.toString()
@@ -195,14 +225,8 @@ class RegisterActivity : AppCompatActivity() {
         val cpf = eTextCPF.text.toString()
         val phone = eTextPhone.text.toString()
 
-        // Verifica se algum campo está vazio
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty() ||
-            age.isEmpty() || cpf.isEmpty() || phone.isEmpty()
-        ) {
-            return false
-        }
-
-        return true
+        return !(name.isEmpty() || email.isEmpty() || password.isEmpty() ||
+                age.isEmpty() || cpf.isEmpty() || phone.isEmpty())
     }
 
     // Navega para a atividade de login
@@ -214,18 +238,19 @@ class RegisterActivity : AppCompatActivity() {
 
     // Verifica se o e-mail é válido
     private fun isValidEmail(email: String): Boolean {
+        // Verifica se o formato do e-mail é válido usando uma expressão regular
         val emailRegex = Regex("^\\S+@\\S+\\.\\S+\$")
         return emailRegex.matches(email)
     }
 
     // Constante para TAG de registro
     companion object {
-        private const val TAG = "RegisterActivity"
+        const val TAG = "RegisterActivity"
     }
 }
 
 // Classe de dados que representa um cliente
-data class Client (
+data class Client(
     val nome: String,
     val email: String,
     val senha: String,
