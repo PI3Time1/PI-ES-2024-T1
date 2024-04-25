f5f9428f20e785f15a77e65783e717733774839b
Luiz Gustavo <jhhhhhhh58@gmail.com>
2024-04-25 00:02:17 -0300

    up

--- app/src/main/java/br/com/sentinellock/TelaQrcodeActivity.kt
+++ app/src/main/java/br/com/sentinellock/TelaQrcodeActivity.kt
@@ -1,5 +1,8 @@
 package br.com.sentinellock
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.SurfaceHolder
@@ -12,28 +15,32 @@ import com.journeyapps.barcodescanner.BarcodeEncoder
 import java.util.Locale
 
 class TelaQrcodeActivity : AppCompatActivity() {
+
+    private var preco: Int = 0
+    private var tempo: Int = 0
+    private var qrCodeData: String = ""
+    private var isAppInForeground = true
+    private var isConfirmationDialogShown = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_qrcode2)
 
-        val preco = intent.getIntExtra("preco", 0)
-        val tempo = intent.getIntExtra("tempo", 0)
+        preco = savedInstanceState?.getInt("preco") ?: intent.getIntExtra("preco", 0)
+        tempo = savedInstanceState?.getInt("tempo") ?: intent.getIntExtra("tempo", 0)
 
         val textView: TextView = findViewById(R.id.textView)
 
-        // Atualizando o texto do TextView com as informações recebidas
-        val tempoText = if (tempo > 4) {
+        val tempoText = if (tempo > 16) {
             String.format(Locale.getDefault(), "%d Minutos", tempo)
         } else {
             String.format(Locale.getDefault(), "%d Horas", tempo)
         }
         textView.text = String.format(Locale.getDefault(), "Preço: R$%d, Tempo: %s", preco, tempoText)
 
-        // Gerando o QR code com as informações de preço e tempo
-        val qrCodeData = String.format(Locale.getDefault(), "Preço: R$%d, Tempo: %d", preco, tempo)
+        qrCodeData = "Preço: ${preco}, Tempo: ${tempo}"
         val qrCodeBitmap = generateQRCode(qrCodeData)
 
-        // Exibindo o QR code na SurfaceView
         val qrCodeSurfaceView: SurfaceView = findViewById(R.id.qrCodeScannerView)
         val holder = qrCodeSurfaceView.holder
         holder.addCallback(object : SurfaceHolder.Callback {
@@ -49,14 +56,63 @@ class TelaQrcodeActivity : AppCompatActivity() {
         })
     }
 
+    override fun onResume() {
+        super.onResume()
+        isAppInForeground = true
+        if (!isAppInForeground && isConfirmationDialogShown) {
+            showConfirmationDialog()
+        }
+        isAppInForeground = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isAppInForeground = false
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (!isAppInForeground) {
+            showConfirmationDialog()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("preco", preco)
+        outState.putInt("tempo", tempo)
+        super.onSaveInstanceState(outState)
+    }
+
     private fun generateQRCode(data: String): Bitmap {
         val barcodeEncoder = BarcodeEncoder()
         return try {
-            val bitMatrix = barcodeEncoder.encodeBitmap(data, BarcodeFormat.QR_CODE, 750, 750)
-            bitMatrix
+            barcodeEncoder.encodeBitmap(data, BarcodeFormat.QR_CODE, 750, 750)
         } catch (e: WriterException) {
             e.printStackTrace()
-            Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888)
+            Bitmap.createBitmap(600, 600, Bitmap.Config.ARGB_8888)
         }
+    }
+
+    private fun showConfirmationDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Continuar compra?")
+        alertDialog.setMessage("Deseja continuar a compra com as informações anteriores?")
+        alertDialog.setPositiveButton("Sim") { dialogInterface: DialogInterface, _: Int ->
+            // Continuar com a compra, não precisa fazer nada aqui
+            dialogInterface.dismiss()
+        }
+        alertDialog.setNegativeButton("Cancelar") { dialogInterface: DialogInterface, _: Int ->
+            // Cancelar a compra e voltar para a tela anterior
+            dialogInterface.dismiss()
+            val intent = Intent(this, TelaArmarioActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        alertDialog.setCancelable(false) // Impede que o usuário clique fora do dialog para fechá-lo
+        alertDialog.setOnDismissListener {
+            isConfirmationDialogShown = false
+        }
+        alertDialog.show()
+        isConfirmationDialogShown = true
     }
 }
